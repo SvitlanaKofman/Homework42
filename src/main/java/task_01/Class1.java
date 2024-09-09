d86af1966cd6caa0a179d1eff4e4cac9b38445e7
SvitlanaKofman <8081992sveta@gmail.com>
2024-09-09 21:34:20 +0200

    Task_1

--- src/main/java/task_01/Class1.java
+++ src/main/java/task_01/Class1.java
@@ -1,7 +1,7 @@
 package task_01;
 
 public class Class1 {
-    public void print() {
-        System.out.println("Class 1");
+    public static void main(String[] args) {
+        System.out.println("Class 4");
     }
 }
